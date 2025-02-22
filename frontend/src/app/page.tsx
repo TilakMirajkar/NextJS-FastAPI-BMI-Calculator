f49e4c61efd859cc7af15911dde63b0653bb6540
TilakMirajkar <teeluck2003@gmail.com>
2025-02-22 19:21:31 +0530

    bug fixes

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -8,11 +8,11 @@ export default function Home() {
   const [bmi, setBmi] = useState(null);
   const [classification, setClassification] = useState(null);
 
-  const handleChange = (e) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setformData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     const response = await fetch("http://127.0.0.1:8000/calculate", {
